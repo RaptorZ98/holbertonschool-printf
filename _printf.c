cff556976210fda41019ae3b4510a4b426db35e8
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-28 06:06:45 -0700

    added main print

---
+++ _printf.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _print - prints
+ * @format: the things to print
+ * Return: the number of characters printed
+ */
+
+int _print(const char *format, ...)
+{
+	int n, i;
+	list pct[] {
+		{"c", get_c},
+		{"s", get_s},
+		{"d", get_d},
+		{"i", get_i}
+	};
+	struct Buff buff;
+	va_list pf;
+
+	buff.length = 0;
+	if (format == NULL)
+		return (0);
+	va_start(pf, format);
+	j = 0;
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while (format[i] == " ")
+				i++;
+			for (n = 0; n < 4; n++)
+			{
+				if (format[i] == pct[n].op)
+				{
+					pct[n].f(pf, &buff);
+					i++;
+					break;
+				}
+			}
+		}
+		else
+		{
+			buff.arr[buff.length] = format[i];
+			buff.length += 1;
+			i++;
+		}
+	}
+	write(1, buff.arr, (buff.length + 1));
+	return (buff.length + 1);
+}
---
+++ main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct Buff - A struct for the buffer and its used length
+ * @length: used length of the buffer
+ * @buff: the buffer
+ */
+typedef struct Buff
+{
+	int length; 
+	char arr[1024];
+} Buff;
+
+/**
+ * struct les - a struct for the functions to use in each case
+ * @op: the char to detect
+ * @f: the function to use if op detected
+ */
+typedef struct les
+{
+	char op;
+	void (*f)(va_list a, struct Buff *buff);
+} lists;
+
+int _print(const char *format, ...);
+
+void get_c(va_list a, struct Buff *buff);
+void get_s(va_list a, struct Buff *buff);
+void get_d(va_list a, struct Buff *buff);
+void get_i(va_list a, struct Buff *buff);
+
+#endif
