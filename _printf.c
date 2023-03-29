0a349840d52f553411da30a77da9b72dd72a33fb
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-29 08:55:09 -0700

    m

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 	va_list pf;
 
 	buff.length = 0;
-	if (format == NULL || (format[0] == '%' && format[1] == '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(pf, format);
 	read_format(format, pf, &buff);
