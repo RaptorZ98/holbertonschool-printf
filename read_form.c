627581526e5063ed9914b4bb5419f2933285f58d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-29 06:33:38 -0700

    m

--- read_form.c
+++ read_form.c
@@ -50,11 +50,6 @@ int read_porcent(int i, const char *format, struct Buff *buff, va_list a)
 	{
 		if (format[i] == '%')
 			return (get_per(buff, i));
-		if (format[i] == ' ')
-		{
-			i++;
-			return (i);
-		}
 		for (n = 0; n < 4; n++)
 		{
 			if (format[i] == pct[n].op)
